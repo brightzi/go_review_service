305bc95603b02b8f949dc9912824ff137fd0cedf
brightzi <zimingming1996@gmail.com>
2024-12-16 09:42:23 +0800

    支持运营端服务发现

--- review_o/internal/data/data.go
+++ review_o/internal/data/data.go
@@ -2,18 +2,22 @@ package data
 
 import (
 	"context"
+	"fmt"
 	v1 "review_o/api/review/v1"
 	"review_o/internal/conf"
 
+	"github.com/go-kratos/kratos/contrib/registry/consul/v2"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/middleware/validate"
+	"github.com/go-kratos/kratos/v2/registry"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
 	"github.com/google/wire"
+	"github.com/hashicorp/consul/api"
 )
 
 // ProviderSet is data providers.
-var ProviderSet = wire.NewSet(NewData, NewOperationRepo, NewReviewServiceClient)
+var ProviderSet = wire.NewSet(NewData, NewOperationRepo, NewReviewServiceClient, NewDiscovery)
 
 // Data .
 type Data struct {
@@ -29,10 +33,13 @@ func NewData(c *conf.Data, rc v1.ReviewClient, logger log.Logger) (*Data, func()
 	return &Data{rc: rc}, cleanup, nil
 }
 
-func NewReviewServiceClient() v1.ReviewClient {
+func NewReviewServiceClient(d registry.Discovery, conf *conf.Servicename) v1.ReviewClient {
+	address := fmt.Sprintf("discovery:///%s", conf.Servername)
 	conn, err := grpc.DialInsecure(
 		context.Background(),
-		grpc.WithEndpoint("127.0.0.1:9000"),
+		// grpc.WithEndpoint("127.0.0.1:9000"),
+		grpc.WithEndpoint(address),
+		grpc.WithDiscovery(d),
 		grpc.WithMiddleware(
 			recovery.Recovery(),
 			validate.Validator(),
@@ -42,4 +49,18 @@ func NewReviewServiceClient() v1.ReviewClient {
 		panic(err)
 	}
 	return v1.NewReviewClient(conn)
+}
+
+func NewDiscovery(conf *conf.Registry) registry.Discovery {
+	c := api.DefaultConfig()
+	c.Address = conf.Consul.Address // 使用配置文件中的值
+	c.Scheme = conf.Consul.Scheme
+
+	client, err := api.NewClient(c)
+	if err != nil {
+		panic(err)
+	}
+	// new reg with consul client
+	dis := consul.New(client)
+	return dis
 }
